e838ec8def602dc1402fa1b3d6060f7983c3aecb
lornd <lornd@qq.com>
2025-06-24 15:32:44 +0800

    项目初始化

---
+++ agent.py
@@ -0,0 +1,13 @@
+# 把自己负责的部分代码迁移到这里，要求写明白自己负责的部分需要的输入输出。
+
+def example(input):
+    """
+    描述一下这个功能模块所干的事情。
+    args:
+        arg_name: 参数描述
+    returns:
+        return_name: 输出描述
+    """
+    # do someting
+    output = input
+    return output
